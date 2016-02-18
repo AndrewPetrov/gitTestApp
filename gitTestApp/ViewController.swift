3e43bebecd711ab67adbb30cec733ffd8479a35a
AndrewPetrov <andrew.petrov@yalantis.com>
2016-02-18 22:01:56 +0200

    comment ot commit 1

---
+++ gitTestApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  gitTestApp
+//
+//  Created by AndrewPetrov on 1/19/16.
+//  Copyright © 2016 AndrewPetrov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // 1 coment
+    // 2 coment
+    // 3 coment
+}
+
